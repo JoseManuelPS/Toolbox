8f53ab3b75fb72a1280b310b483a740959619809
José Manuel Plana Santos <dev.josemanuelps@gmail.com>
2021-05-30 12:59:41 +0200

    [custom_youtube-dl] Pre-Release v1.0

---
+++ custom_youtube-dl/custom_youtube-dl.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+##        Name: custom_youtube-dl.py                                          #
+##        Date: 30/05/2021                                                    #
+## Description: Assistant to use the youtube-dl tool.                         #
+##----------------------------------------------------------------------------#
+##      Editor: José Manuel Plana Santos                                      #
+##     Contact: dev.josemanuelps@gmail.com                                    #
+###############################################################################
+
+
+
+###############################################################################
+## Imports.  ##################################################################
+###############################################################################
+
+import re
+import argparse
+import subprocess
+from os import path
+from os import listdir
+
+
+
+###############################################################################
+## Args. ######################################################################
+###############################################################################
+
+parser = argparse.ArgumentParser(description = 'Assistant to use the youtube-dl tool.')
+parser.add_argument('path', metavar = 'PATH', type = str, help = 'Set the path to the directory where the download is saved.')
+parser.add_argument('--name', type = str, help = 'Set the name under which the download is saved.')
+args = parser.parse_args()
+
+
+
+###############################################################################
+## Main code. #################################################################
+###############################################################################
+
+# Checking if the application youtube-dl exists and update it.
+try:
+  p = subprocess.Popen(['youtube-dl', "-U"])
+  p.wait()
+except:
+  print ('\n\n[ERROR] The application youtube-dl is not installed.')
+  exit(1)
+
+# Fixing possible incorrect path.
+args.path = path.abspath(args.path)
+if not path.isdir(args.path):
+  print ('\n\n[ERROR] <(' + args.path + ')> is not a directory.')
+  exit(1)
+if args.path[len(args.path) - 1] != '/':
+  args.path += '/'
+
+# Printing instructions.
+p = subprocess.Popen(['clear', '-x'])
+p.wait()
+print('Type <(exit)> to quit this program')
+
+# Load the number of episodes if the name is written.
+if args.name is not None:
+  episode_list = []
+  for x in listdir(args.path):
+    if path.isfile(args.path + x):
+      regular_expression = re.search('(?<=' + args.name + ')\\w+', x)
+      if regular_expression is not None:
+        number_of_episode = regular_expression.group(0)
+        episode_list.append(int(number_of_episode))
+
+  # Checking for new episode number.
+  if not episode_list:
+    episode_num = 0
+  else:
+    episode_num = max(episode_list)
+
+while 1:
+  url = input('\n\nURL: ')
+  if url == 'exit':
+    exit(0)
+
+  if args.name is not None:
+    episode_num += 1
+    print('Downloading as: ' + args.name + "_" + str(episode_num))
+    full_name = args.path + args.name + "_" + str(episode_num) + '.%(ext)s'
+
+  # Executing youtube-dl
+  if not args.name:
+    p = subprocess.Popen(['youtube-dl', url])
+    p.wait()
+  else:
+    p = subprocess.Popen(['youtube-dl', '-o', full_name, url])
+    p.wait()
+
+
